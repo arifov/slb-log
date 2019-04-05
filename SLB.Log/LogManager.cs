5816fd38987ed54d091d3554475027b06dbf312a
Asan Arifov <asan.arifov@mcpayment.com>
2019-04-05 11:40:45 +0800

    Update : SLB.Log. Close and Flush properly

--- SLB.Log/LogManager.cs
+++ SLB.Log/LogManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Serilog;
-using Serilog.Core;
 
 namespace SLB.Log
 {
@@ -10,158 +9,156 @@ namespace SLB.Log
     /// </summary>
     public class LogManager: ILogManager
     {
-        private readonly Logger _logger;
-        
         public LogManager(IConfiguration configuration)
         {
-            _logger = new LoggerConfiguration()
+            Serilog.Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
         }
 
         public void Dispose()
         {
-            _logger.Dispose();
+            Serilog.Log.CloseAndFlush();
         }
 
         public void Write(LogLevel level, string message)
         {
-            _logger.Write(level.ToLogEventLevel(), message);
+            Serilog.Log.Write(level.ToLogEventLevel(), message);
         }
 
         public void Write(LogLevel level, string message, params object[] propertyValues)
         {
-            _logger.Write(level.ToLogEventLevel(), message, propertyValues);
+            Serilog.Log.Write(level.ToLogEventLevel(), message, propertyValues);
         }
 
         public void Write(LogLevel level, string message, Exception exception)
         {
-            _logger.Write(level.ToLogEventLevel(), exception, message);
+            Serilog.Log.Write(level.ToLogEventLevel(), exception, message);
         }
 
         public void Write(LogLevel level, string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Write(level.ToLogEventLevel(), exception, message, propertyValues);
+            Serilog.Log.Write(level.ToLogEventLevel(), exception, message, propertyValues);
         }
 
         public void Verbose(string message)
         {
-            _logger.Verbose(message);
+            Serilog.Log.Verbose(message);
         }
 
         public void Verbose(string message, params object[] propertyValues)
         {
-            _logger.Verbose(message, propertyValues);
+            Serilog.Log.Verbose(message, propertyValues);
         }
 
         public void Verbose(string message, Exception exception)
         {
-            _logger.Verbose(exception, message);
+            Serilog.Log.Verbose(exception, message);
         }
 
         public void Verbose(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Verbose(exception, message, propertyValues);
+            Serilog.Log.Verbose(exception, message, propertyValues);
         }
 
         public void Debug(string message)
         {
-            _logger.Debug(message);
+            Serilog.Log.Debug(message);
         }
 
         public void Debug(string message, params object[] propertyValues)
         {
-            _logger.Debug(message, propertyValues);
+            Serilog.Log.Debug(message, propertyValues);
         }
 
         public void Debug(string message, Exception exception)
         {
-            _logger.Debug(exception, message);
+            Serilog.Log.Debug(exception, message);
         }
 
         public void Debug(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Debug(exception, message, propertyValues);
+            Serilog.Log.Debug(exception, message, propertyValues);
         }
 
         public void Information(string message)
         {
-            _logger.Information(message);
+            Serilog.Log.Information(message);
         }
 
         public void Information(string message, params object[] propertyValues)
         {
-            _logger.Information(message, propertyValues);
+            Serilog.Log.Information(message, propertyValues);
         }
 
         public void Information(string message, Exception exception)
         {
-            _logger.Information(exception, message);
+            Serilog.Log.Information(exception, message);
         }
 
         public void Information(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Information(exception, message, propertyValues);
+            Serilog.Log.Information(exception, message, propertyValues);
         }
 
         public void Warning(string message)
         {
-            _logger.Warning(message);
+            Serilog.Log.Warning(message);
         }
 
         public void Warning(string message, params object[] propertyValues)
         {
-            _logger.Warning(message, propertyValues);
+            Serilog.Log.Warning(message, propertyValues);
         }
 
         public void Warning(string message, Exception exception)
         {
-            _logger.Warning(exception, message);
+            Serilog.Log.Warning(exception, message);
         }
 
         public void Warning(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Warning(exception, message, propertyValues);
+            Serilog.Log.Warning(exception, message, propertyValues);
         }
 
         public void Error(string message)
         {
-            _logger.Error(message);
+            Serilog.Log.Error(message);
         }
 
         public void Error(string message, params object[] propertyValues)
         {
-            _logger.Error(message, propertyValues);
+            Serilog.Log.Error(message, propertyValues);
         }
 
         public void Error(string message, Exception exception)
         {
-            _logger.Error(exception, message);
+            Serilog.Log.Error(exception, message);
         }
 
         public void Error(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Error(exception, message, propertyValues);
+            Serilog.Log.Error(exception, message, propertyValues);
         }
 
         public void Fatal(string message)
         {
-            _logger.Fatal(message);
+            Serilog.Log.Fatal(message);
         }
 
         public void Fatal(string message, params object[] propertyValues)
         {
-            _logger.Fatal(message, propertyValues);
+            Serilog.Log.Fatal(message, propertyValues);
         }
 
         public void Fatal(string message, Exception exception)
         {
-            _logger.Fatal(exception, message);
+            Serilog.Log.Fatal(exception, message);
         }
 
         public void Fatal(string message, Exception exception, params object[] propertyValues)
         {
-            _logger.Fatal(exception, message, propertyValues);
+            Serilog.Log.Fatal(exception, message, propertyValues);
         }
     }
 }
